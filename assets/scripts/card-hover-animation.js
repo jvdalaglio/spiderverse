7d5dd5d2941f9dd89d98173a959180ccb1502f61
jvdalaglio <jvdalaglio@gmail.com>
2022-11-30 22:41:15 -0300

    Adicionando eventos e transições nos carda com JS

---
+++ assets/scripts/card-hover-animation.js
@@ -0,0 +1,19 @@
+function handleMouseEnter() {
+  this.classList.add('s-card--hovered');
+}
+
+function handleMouseLeave() {
+  this.classList.remove('s-card--hovered');
+}
+
+function addEventListenersToCards() {
+  const cardElements = document.getElementsByClassName('s-card');
+  
+  for (let i = 0; i < cardElements.length; i++) {
+    const card = cardElements[i];
+    card.addEventListener('mouseenter', handleMouseEnter);
+    card.addEventListener('mouseleave', handleMouseLeave);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', addEventListenersToCards, false);
